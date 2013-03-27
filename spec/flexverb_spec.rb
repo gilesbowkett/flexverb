6362fe1c5be3a370e1dde2740a9a95f449045572
Giles Bowkett <giles.bowkett@gmail.com>
2013-03-27 14:30:16 -0700

    spec clarity

--- spec/flexverb_spec.rb
+++ spec/flexverb_spec.rb
@@ -2,30 +2,34 @@ require 'flexverb'
 
 describe FlexVerb do
 
-  it "transforms a direct object" do
-    xform = FlexVerb::Transform.new
-    expect(xform.apply(:direct_object => '"hello world"')).to eq('hello world')
+  context "Transform" do
+    it "transforms a direct object" do
+      xform = FlexVerb::Transform.new
+      expect(xform.apply(:direct_object => '"hello world"')).to eq('hello world')
+    end
+
+    it "transforms a verb" do
+      xform = FlexVerb::Transform.new
+      expect(xform.apply(:verb => 'print')).to eq(:puts)
+    end
   end
 
-  it "transforms a verb" do
-    xform = FlexVerb::Transform.new
-    expect(xform.apply(:verb => 'print')).to eq(:puts)
-  end
+  context "Interpreter" do
+    it "executes a method" do
+      abstract_syntax_tree = [{:verb => "print"}, {:direct_object => '"hello world"'}]
+      interpreter = FlexVerb::Interpreter.new(abstract_syntax_tree)
 
-  it "executes a method" do
-    abstract_syntax_tree = [{:verb => "print"}, {:direct_object => '"hello world"'}]
-    interpreter = FlexVerb::Interpreter.new(abstract_syntax_tree)
+      Kernel.should_receive(:puts).with "hello world"
+      interpreter.interpret
+    end
 
-    Kernel.should_receive(:puts).with "hello world"
-    interpreter.interpret
-  end
+    it "ignores word order" do
+      abstract_syntax_tree = [{:direct_object => '"hello world"'}, {:verb => "print"}]
+      interpreter = FlexVerb::Interpreter.new(abstract_syntax_tree)
 
-  it "ignores word order" do
-    abstract_syntax_tree = [{:direct_object => '"hello world"'}, {:verb => "print"}]
-    interpreter = FlexVerb::Interpreter.new(abstract_syntax_tree)
-
-    Kernel.should_receive(:puts).with "hello world"
-    interpreter.interpret
+      Kernel.should_receive(:puts).with "hello world"
+      interpreter.interpret
+    end
   end
 
 end
