c586c7bd29e7f7ec7004cff0ea0f0a72ef31b0b1
Giles Bowkett <giles.bowkett@gmail.com>
2013-03-27 14:27:17 -0700

    refactor - marginally worse, but more consistent
    it's more repetitive and primitive, but it's more like the
    subsequent interpreter tests, and this project may not ever
    get big enough to merit separate tests each for the interpreter,
    transform, and parser (which doesn't even exist yet). so for now,
    it's fine.

--- spec/flexverb_spec.rb
+++ spec/flexverb_spec.rb
@@ -1,13 +1,14 @@
 require 'flexverb'
 
 describe FlexVerb do
-  let(:xform) { FlexVerb::Transform.new }
 
   it "transforms a direct object" do
+    xform = FlexVerb::Transform.new
     expect(xform.apply(:direct_object => '"hello world"')).to eq('hello world')
   end
 
   it "transforms a verb" do
+    xform = FlexVerb::Transform.new
     expect(xform.apply(:verb => 'print')).to eq(:puts)
   end
 
