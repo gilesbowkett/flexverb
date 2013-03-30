995354a7589772d91f8a4db0d73e2b5bdda9b48a
Giles Bowkett <giles.bowkett@gmail.com>
2013-03-30 09:55:58 -0700

    cleaner specs

--- spec/flexverb_spec.rb
+++ spec/flexverb_spec.rb
@@ -1,16 +1,17 @@
 require 'flexverb'
 
 describe FlexVerb do
+  before do
+    @transform = FlexVerb::Transform.new
+  end
 
   context "Transform" do
     it "transforms a direct object" do
-      xform = FlexVerb::Transform.new
-      expect(xform.apply(:direct_object => '"hello world"')).to eq('hello world')
+      expect(@transform.apply(:direct_object => '"hello world"')).to eq('hello world')
     end
 
     it "transforms a verb" do
-      xform = FlexVerb::Transform.new
-      expect(xform.apply(:verb => 'print')).to eq(:puts)
+      expect(@transform.apply(:verb => 'print')).to eq(:puts)
     end
   end
 
@@ -39,7 +40,8 @@ describe FlexVerb do
 
     context "with a complete line of code" do
       before do
-        @terms = [{:verb => "print"}, {:direct_object => '"hello world"'}]
+        @terms = [{:verb => "print"},
+                  {:direct_object => '"hello world"'}]
       end
 
       it "parses" do
@@ -52,14 +54,16 @@ describe FlexVerb do
         expect(parse(code)).to eq(@terms)
       end
 
-      it "ignores term position" do
+      it "still parses if the code is 'reversed'" do
         code = 'direct-object("hello world") verb(print)'
         expect(parse(code)).to eq(@terms.reverse!)
       end
     end
 
     it "recognizes a verb" do
-      expect(parse('verb(print)')).to eq(:verb => 'print')
+      code = 'verb(print)'
+      term = {:verb => 'print'}
+      expect(parse(code)).to eq(term)
     end
 
     it "recognizes a direct object" do
